a2ae25dbe6887f6378567b9bf5b32663f2b74da0
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-11-28 20:26:04 +0600

    portfolio for coursera 2 dark mode

--- coursera/final_portfolio_2/ezaz_portfolio/src/App.js
+++ coursera/final_portfolio_2/ezaz_portfolio/src/App.js
@@ -1,13 +1,27 @@
-
+import { IconButton } from "@chakra-ui/button";
+import { Flex, Heading ,VStack } from "@chakra-ui/layout";
+import { useColorMode } from "@chakra-ui/react";
+import { color } from "framer-motion";
+import { useCallback } from "react";
+import {FaSun, FaMoon, FaInstagram, FaGithub, FaLinkedIn} from "react-icons/fa";
 
 
 function App() {
-  return (
-    <div className="App">
-      Ezaz Portfolio
 
-  
-    </div>
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode == "dark";
+
+  return (
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading ml="8" size="md" fontWeight="semibold" color='cyan.400'> 
+          ebtesham portfolio
+
+        </Heading>
+
+      </Flex>
+      <IconButton ml={8} icon={isDark ? <FaSun/> : <FaMoon/> } onClick={toggleColorMode}  isRound="true"></IconButton>
+    </VStack>
   );
 }
 
--- coursera/final_portfolio_2/ezaz_portfolio/src/index.js
+++ coursera/final_portfolio_2/ezaz_portfolio/src/index.js
@@ -1,14 +1,20 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <React.StrictMode>
+    <ChakraProvider>
+      <ColorModeScript initialColorMode="light">
+      </ColorModeScript>
+      <App/>
+    </ChakraProvider>
+ </React.StrictMode>, 
+ document.getElementById('root')
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
