86e7da1ed4882c5084090d9c92f02ff56277df30
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-12-11 17:19:10 +0600

    hstack error

--- coursera/react-advanced-final/src/components/Header.js
+++ coursera/react-advanced-final/src/components/Header.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import HStack from '@chakra-ui/react';
 import { Link } from 'react-scroll'; 
 
 const Header = () => {
     return (
         <header>
             <nav>
-                <HStack >
+                <HStack spacing = {8} >
                     {
                         socials.map((social) =>
-                        <a href={social.url}>
-                            
+                        <a href={social.url} key={social.url}
+                        >
+                            <FontAwesomeIcon icon={social.icon} size="2x" />
+
                         </a>
                         )
                     }
                     
+                </HStack>
+            </nav>
+            <nav>
+                <HStack spacing={8}>
+                    <a href="#projects-section" onClick={handleClick("projects")}>
+                        Projects
+                    </a>
+                    <a href="contactme-section" onClick={handleClick("projects")>
+                        Contact Me
+                    </a>
                 </HStack>
             </nav>
         </header>
