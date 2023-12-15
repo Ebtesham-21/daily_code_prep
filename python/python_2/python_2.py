42a1a2b191850c22245ca59a7a3a9b53558a7fc5
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-12-15 20:32:14 +0600

    while python

--- python/python_2/python_2.py
+++ python/python_2/python_2.py
@@ -1,18 +1,38 @@
+# import turtle
+
+# turtle = turtle.Turtle()
+
+
+# def square():
+#   turtle.forward(90)
+#   turtle.left(90)
+#   turtle.forward(90)
+#   turtle.left(90)
+#   turtle.forward(90)
+#   turtle.left(90)
+#   turtle.forward(90)
+
+
+# square()
+# turtle.forward(90)
+# square()
+
+
 import turtle
 
 turtle = turtle.Turtle()
 
 
-def square():
-  turtle.forward(90)
+def square(length):
+  turtle.forward(length)
   turtle.left(90)
-  turtle.forward(90)
+  turtle.forward(length)
   turtle.left(90)
-  turtle.forward(90)
+  turtle.forward(length)
   turtle.left(90)
-  turtle.forward(90)
+  turtle.forward(length)
 
 
-square()
-turtle.forward(90)
-square()
+square(200)
+
+square(200)
