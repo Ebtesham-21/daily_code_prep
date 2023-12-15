792dc2734379fb3e4dd798e87ce490e277846704
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-12-15 11:50:12 +0600

    python_turtle_prb 2

--- python/python_2/python_2.py
+++ python/python_2/python_2.py
@@ -2,10 +2,17 @@ import turtle
 
 turtle = turtle.Turtle()
 
+
+def square():
+  turtle.forward(90)
+  turtle.left(90)
+  turtle.forward(90)
+  turtle.left(90)
+  turtle.forward(90)
+  turtle.left(90)
+  turtle.forward(90)
+
+
+square()
 turtle.forward(90)
-turtle.left(90)
-turtle.forward(90)
-turtle.left(90)
-turtle.forward(90)
-turtle.left(90)
-turtle.forward(90)
+square()
