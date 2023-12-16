da35be38eaea92b40fb6afb73b3c84837cb3d3a7
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-12-16 23:51:25 +0600

    python sliecd

---
+++ python/python_2/python_3.py
@@ -0,0 +1,20 @@
+# i = 0
+# while i < 50:
+#     print(i)
+#     i = i + 1
+
+# p = 100
+# while p > 0:
+#     print(p)
+#     p = p - 1 
+#     if p == 0:
+#         print("Boom")
+
+
+for p in range(0,101):
+    if(p > 0):
+        print(p)
+    else:
+        print("Boom")
+
+ 
